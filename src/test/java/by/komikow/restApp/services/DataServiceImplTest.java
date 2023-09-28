d0bc9d934b8491a29ac475b7607cac517a45599b
Admin <komikow@mail.ru>
2023-09-28 21:27:05 +0300

    add code with test

---
+++ src/test/java/by/komikow/restApp/services/DataServiceImplTest.java
@@ -0,0 +1,29 @@
+package by.komikow.restApp.services;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@ExtendWith(MockitoExtension.class)
+class DataServiceImplTest {
+    @Mock
+    DataService dataServiceImpl;
+
+    @Test
+    void testGetStringFromNull() {
+        String request = null;
+        String result = dataServiceImpl.getCountChars(request);
+        assertNull(result);
+    }
+
+    @Test
+    void testGetStringFromString() {
+        String request = "hello";
+        String response = dataServiceImpl.getCountChars(request);
+        assertNotSame(response, request);
+    }
+}
