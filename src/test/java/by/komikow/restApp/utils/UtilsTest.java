b1ec6ac26acd645de7e1efadbb29612d5e5e8e46
Admin <komikow@mail.ru>
2023-09-28 22:10:54 +0300

    fixed the test

--- src/test/java/by/komikow/restApp/utils/UtilsTest.java
+++ src/test/java/by/komikow/restApp/utils/UtilsTest.java
@@ -3,11 +3,7 @@ package by.komikow.restApp.utils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.stream.IntStream;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author Sergey Komikov
@@ -30,24 +26,5 @@ class UtilsTest {
         String expectedString = "{1=4, 2=3, 3=2}";
         String response = Utils.getCharsMap(String.valueOf(numberRequest));
         assertEquals(expectedString, response);
-    }
-
-    @Test
-    void testGetSortingMap() {
-        String request = "Application";
-        String[] arrayChars = request.split("");
-        Map<String, Integer> beforeSortingMap = new HashMap<>();
-        IntStream.range(0, arrayChars.length).forEachOrdered(i -> {
-            if (beforeSortingMap.containsKey(arrayChars[i])) {
-                int counter = beforeSortingMap.get(arrayChars[i]);
-                beforeSortingMap.put(arrayChars[i], counter + 1);
-            } else {
-                beforeSortingMap.put(arrayChars[i], 1);
-            }
-        });
-        Map<String, Integer> afterSortingMap = Utils.sortByValue(beforeSortingMap);
-        assertNotNull(beforeSortingMap);
-        assertNotNull(afterSortingMap);
-        assertNotSame(afterSortingMap, beforeSortingMap);
     }
 }
